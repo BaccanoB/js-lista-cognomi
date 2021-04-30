9aa0fe0b78e13e459a2f003932e21bfac16d7ed1
BaccanoB <beatrice.baccano@yahoo.it>
2021-04-30 14:52:43 +0200

    richiesta cognome e inserimento nell'array

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// 1. chiedi all'utente il cognome
+// 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+var listaCognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+listaCognomi.push(prompt("Inserisci qui il tuo cognome"));
+console.log(listaCognomi);
+
+// 3. stampa la lista ordinata alfabeticamente
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
