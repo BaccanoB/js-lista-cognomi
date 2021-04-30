a1225991120ee65aa7c76377214530311b6557af
BaccanoB <beatrice.baccano@yahoo.it>
2021-04-30 15:57:47 +0200

    inserito ordine alfabetico

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@
 // 1. chiedi all'utente il cognome
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
 var listaCognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+console.log(listaCognomi);
+
 listaCognomi.push(prompt("Inserisci qui il tuo cognome"));
 console.log(listaCognomi);
 
 // 3. stampa la lista ordinata alfabeticamente
 
+listaCognomi.sort();
+
+console.log(listaCognomi);
+
+var i = 1;
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+console.log(listaCognomi.indexOf("Baccano"));
